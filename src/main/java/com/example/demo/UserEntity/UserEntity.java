ece791e6cef43e5da03b39c7f7748d0c1dfa3b6e
Mateusz Redestowicz <mati.red@gmail.com>
2022-10-09 16:41:12 +0200

    added UserEntity

--- src/main/java/com/example/demo/UserEntity/UserEntity.java
+++ src/main/java/com/example/demo/UserEntity/UserEntity.java
@@ -1,2 +1,7 @@
-package com.example.demo.UserEntity;public class UserEntity {
+package com.example.demo.UserEntity;
+
+public class UserEntity {
+    public static void main(String[] args) {
+        System.out.println("polecenie");
+    }
 }
